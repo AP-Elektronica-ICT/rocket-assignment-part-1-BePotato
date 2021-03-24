36d20af4f0fed97761ee218d14dcab91047c6422
Max <maximilian.senten00@gmail.com>
2021-03-24 15:02:02 +0100

    Level + level2

---
+++ Rocket Assignment/Assets/Script/MyGameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameManager : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject MainCanvas;
+    public GameObject GameOverCanvas;
+    private Health healthPlayer;
+    public enum GameStates 
+    { 
+        Playing,
+        GameOver
+    }
+    public GameStates gameState = GameStates.Playing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Player == null)
+        {
+            Player = GameObject.FindWithTag("Player");
+        }
+        healthPlayer = Player.GetComponent<Health>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //State Machine of the game
+        switch (gameState)
+        {
+            case GameStates.Playing:
+                if (healthPlayer.isAlive == false)
+                {
+                    gameState = GameStates.GameOver;
+                    MainCanvas.SetActive(false);
+                    GameOverCanvas.SetActive(true);
+                }
+                break;
+           
+        }
+    }
+}
